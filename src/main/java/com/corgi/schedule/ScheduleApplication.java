df2447163827724ade88727b629ae15b7f96703a
tairanliu <dasltr@126.com>
2020-01-07 14:41:49 +0800

    init

--- src/main/java/com/corgi/schedule/ScheduleApplication.java
+++ src/main/java/com/corgi/schedule/ScheduleApplication.java
@@ -1,5 +1,6 @@
 package com.corgi.schedule;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableDubboConfiguration
 public class ScheduleApplication {
 
 	public static void main(String[] args) {
