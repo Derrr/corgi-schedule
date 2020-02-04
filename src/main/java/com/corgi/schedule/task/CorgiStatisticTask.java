e00cecf9d5d505603653a616db98de4bb378cadb
tairanliu <dasltr@126.com>
2020-02-04 13:50:15 +0800

    fix bug

--- src/main/java/com/corgi/schedule/task/CorgiStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiStatisticTask.java
@@ -69,7 +69,7 @@ public class CorgiStatisticTask {
             for (HashMap hashMap : hashMapList) {
                 try {
                     log.info(hashMap.toString());
-                    corgiStatisticService.addList(CorgiStatistic.ACTIVITY_TYPE, date, (String) hashMap.get("activityType"), Long.valueOf((String) hashMap.get("count")));
+                    corgiStatisticService.addList(CorgiStatistic.ACTIVITY_TYPE, date, (String) hashMap.get("_id"),  Long.valueOf(hashMap.get("count").toString()));
                 } catch (Exception e) {
                     log.error(e.getMessage(), e);
                 }
