77ea0ca8cb0b812c48361bce08192ff33b810781
tairanliu <dasltr@126.com>
2020-02-05 11:45:14 +0800

    fix bug

--- src/main/java/com/corgi/schedule/ScheduleApplication.java
+++ src/main/java/com/corgi/schedule/ScheduleApplication.java
@@ -1,8 +1,11 @@
 package com.corgi.schedule;
 
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.corgi.common.CorgiQueueName;
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -15,6 +18,11 @@ public class ScheduleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScheduleApplication.class, args);
+	}
+
+	@Bean
+	public Queue pushMessageQueue() {
+		return new Queue(CorgiQueueName.PUSH_MESSAGE_QUEUE);
 	}
 
 }
