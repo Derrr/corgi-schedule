62052775e1a58a8c87f276db87c54845bf3918da
tairanliu <dasltr@126.com>
2020-02-26 21:39:50 +0800

    fix bug

--- src/main/java/com/corgi/schedule/task/CorgiStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiStatisticTask.java
@@ -42,8 +42,8 @@ public class CorgiStatisticTask {
     private static SimpleDateFormat activity_sdf = new SimpleDateFormat("yyyy/MM/dd");
 
     @Async
-    //@Scheduled(cron = "0 59 23 * * *")
-    @Scheduled(fixedRate = 24 * 3600 * 1000)
+    @Scheduled(cron = "0 59 23 * * *")
+    //@Scheduled(fixedRate = 24 * 3600 * 1000)
     public void run() {
         Calendar calendar = Calendar.getInstance();
         Long time = calendar.getTimeInMillis();
