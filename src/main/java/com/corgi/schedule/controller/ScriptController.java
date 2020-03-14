a8e31c4ed6aa060796be9b33e0e30593373eef94
tairanliu <dasltr@126.com>
2020-03-14 13:05:20 +0800

    fix bug

--- src/main/java/com/corgi/schedule/controller/ScriptController.java
+++ src/main/java/com/corgi/schedule/controller/ScriptController.java
@@ -1,8 +1,11 @@
 package com.corgi.schedule.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.schedule.service.MapService;
+import com.corgi.user.api.CorgiUserMatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 /**
  * @author tairanliu
  */
@@ -11,9 +14,17 @@ import org.springframework.web.bind.annotation.*;
 public class ScriptController {
     @Autowired
     private MapService mapService;
+    @Reference
+    private CorgiUserMatchService corgiUserMatchService;
 
     @GetMapping("init_station")
     public String initStation(@RequestParam("city") String city) {
         return mapService.initCity(city);
+    }
+
+    @GetMapping("clear_keys")
+    public String clearKeys() {
+        corgiUserMatchService.clearMatch();
+        return "success";
     }
 }
