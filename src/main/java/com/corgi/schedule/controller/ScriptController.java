3580effe4f1dd67be73eca4bf0a507c17f38a576
tairanliu <dasltr@126.com>
2020-04-01 11:39:57 +0800

    fix bug

--- src/main/java/com/corgi/schedule/controller/ScriptController.java
+++ src/main/java/com/corgi/schedule/controller/ScriptController.java
@@ -3,8 +3,17 @@ package com.corgi.schedule.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.schedule.service.MapService;
 import com.corgi.user.api.CorgiUserMatchService;
+import com.corgi.user.api.CorgiUserService;
+import com.corgi.user.entity.UserPosition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author tairanliu
@@ -16,10 +25,41 @@ public class ScriptController {
     private MapService mapService;
     @Reference
     private CorgiUserMatchService corgiUserMatchService;
+    @Reference
+    private CorgiUserService corgiUserService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @GetMapping("init_station")
     public String initStation(@RequestParam("city") String city) {
         return mapService.initCity(city);
+    }
+
+    @GetMapping("refresh_position")
+    public String refreshPosition() {
+        List<UserPosition> userPositionList;
+        int page = 1;
+        int pageSize = 1000;
+        do {
+            userPositionList = corgiUserService.getUserPositionByPage(page, pageSize);
+            page++;
+            if (userPositionList != null) {
+                for (UserPosition userPosition : userPositionList) {
+                    redisTemplate.opsForGeo().remove("user",userPosition.getUserId());
+                    if (userPosition.getLng() == null || userPosition.getLng() > 180 || userPosition.getLng() < -180) {
+                        continue;
+                    }
+                    if (userPosition.getLat() == null || userPosition.getLat() > 90 || userPosition.getLat() < -90) {
+                        continue;
+                    }
+                    if (StringUtils.isEmpty(userPosition.getUserId())) {
+                        continue;
+                    }
+                    redisTemplate.opsForGeo().add("user", new Point(userPosition.getLng(), userPosition.getLat()), userPosition.getUserId());
+                }
+            }
+        } while (!CollectionUtils.isEmpty(userPositionList));
+        return "success";
     }
 
     @GetMapping("clear_keys")
