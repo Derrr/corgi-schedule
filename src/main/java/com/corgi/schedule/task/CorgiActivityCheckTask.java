22b300970deaf64f8b0519b28671d250f3f8eabc
tairanliu <dasltr@126.com>
2020-04-05 09:22:30 +0800

    添加定时检测推送

--- src/main/java/com/corgi/schedule/task/CorgiActivityCheckTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiActivityCheckTask.java
@@ -56,7 +56,6 @@ public class CorgiActivityCheckTask {
 
     @Scheduled(cron = "0 * * * * *")
     public void run() {
-        log.info("running activity check...");
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.HOUR_OF_DAY, 1);
         String endDate = sdf.format(calendar.getTime());
@@ -70,7 +69,6 @@ public class CorgiActivityCheckTask {
         IAcsClient client = new DefaultAcsClient(profile);
         do {
             activityList = corgiActivityService.searchCorgiActivity(query, page, pageSize);
-            log.info("getting activitys:{}", activityList);
             page++;
             if (activityList != null) {
                 for (CorgiActivity activity : activityList) {
@@ -95,7 +93,6 @@ public class CorgiActivityCheckTask {
 
     private void sendMessage(IAcsClient client, CorgiActivity activity, String userId, String telNo, String time) {
         String key = "activity_check_sent_" + activity.getId() + "_" + userId;
-        log.info("checking key..." + key + redisTemplate.hasKey(key));
         if (!redisTemplate.hasKey(key)) {
             CommonRequest request = new CommonRequest();
             request.setMethod(MethodType.POST);
