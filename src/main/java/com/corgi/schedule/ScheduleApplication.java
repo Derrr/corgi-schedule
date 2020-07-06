0a5362c505eaeb7cd88f1f1cb9c82a9b8366269e
tairanliu <dasltr@126.com>
2020-07-06 15:47:56 +0800

    fix bug

--- src/main/java/com/corgi/schedule/ScheduleApplication.java
+++ src/main/java/com/corgi/schedule/ScheduleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableDubboConfiguration
+@EnableAsync
 public class ScheduleApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/corgi/schedule/task/CorgiBillboardTask.java
@@ -0,0 +1,109 @@
+package com.corgi.schedule.task;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.corgi.activity.api.CorgiActivityService;
+import com.corgi.activity.entity.CorgiActivity;
+import com.corgi.schedule.service.MQService;
+import com.corgi.user.api.CorgiBillboardService;
+import com.corgi.user.api.CorgiUserActivityService;
+import com.corgi.user.api.CorgiUserService;
+import com.corgi.user.entity.UserDetail;
+import com.corgi.user.entity.UserLogin;
+import com.corgi.user.entity.UserProfile;
+import com.corgi.user.entity.UserSignUp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author tairanliu
+ */
+@Component
+@Slf4j
+public class CorgiBillboardTask {
+    @Reference
+    private CorgiBillboardService corgiBillboardService;
+
+    @Async
+    @Scheduled(fixedRate = 24 * 3600 * 1000)
+    public void run() {
+        List<String> userIds = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String date = sdf.format(calendar.getTime());
+        UserDetail searchUser = new UserDetail();
+        searchUser.setRole("1");
+        List<UserProfile> userProfiles = corgiBillboardService.getPopularUser(searchUser, 2);
+        for (UserProfile userProfile : userProfiles) {
+            userIds.add(userProfile.getUserId());
+            corgiBillboardService.addBillboard(userProfile, date, "fans1");
+        }
+        searchUser.setRole("0");
+        userProfiles = corgiBillboardService.getPopularUser(searchUser, 2);
+        for (UserProfile userProfile : userProfiles) {
+            userIds.add(userProfile.getUserId());
+            corgiBillboardService.addBillboard(userProfile, date, "fans0");
+        }
+        userProfiles = corgiBillboardService.getPassionUser(searchUser, 6);
+        int i = 0;
+        for (UserProfile userProfile : userProfiles) {
+            if (userIds.contains(userProfile.getUserId())) {
+                continue;
+            }
+            i++;
+            userIds.add(userProfile.getUserId());
+            corgiBillboardService.addBillboard(userProfile, date, "follow");
+            if (i >= 2) {
+                break;
+            }
+        }
+        userProfiles = corgiBillboardService.getActiveUser(searchUser, 10);
+        i = 0;
+        for (UserProfile userProfile : userProfiles) {
+            if (userIds.contains(userProfile.getUserId())) {
+                continue;
+            }
+            i++;
+            userIds.add(userProfile.getUserId());
+            corgiBillboardService.addBillboard(userProfile, date, "active");
+            if (i >= 4) {
+                break;
+            }
+        }
+        if (userIds.size() < 10) {
+            searchUser.setRole(null);
+            userProfiles = corgiBillboardService.getPopularUser(searchUser, 10);
+            for (UserProfile userProfile : userProfiles) {
+                if (userIds.contains(userProfile.getUserId())) {
+                    continue;
+                }
+                i++;
+                userIds.add(userProfile.getUserId());
+                corgiBillboardService.addBillboard(userProfile, date, "active");
+                if (userIds.size() >= 10) {
+                    break;
+                }
+            }
+        }
+    }
+}
