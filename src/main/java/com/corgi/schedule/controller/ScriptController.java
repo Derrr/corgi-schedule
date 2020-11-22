27f891fb8bee3d3f898bc6369af55137caf199a0
tairanliu <dasltr@126.com>
2020-11-22 23:02:01 +0800

    add log

--- src/main/java/com/corgi/schedule/controller/ScriptController.java
+++ src/main/java/com/corgi/schedule/controller/ScriptController.java
@@ -362,6 +362,11 @@ public class ScriptController {
                         List<UserPic> userPics = corgiPicService.getUserPic(userId);
                         if (CollectionUtils.isEmpty(userPics)
                                 && !CorgiPic.NEED_CHECK.equals(userDetail.getAvatarCheckStatus())) {
+                            UserDetail queryDetail = new UserDetail();
+                            queryDetail.setUserId(userId);
+                            UserProfile userProfile = corgiUserService.searchUsers(queryDetail, null, 1, 1).get(0);
+                            UserPosition position = corgiUserService.getUserPosition(userId);
+                            log.info("register time: {} version: {} ", userProfile.getCreateTime(), position.getVersion());
                             UserPic userPic = new UserPic();
                             userPic.setStatus(UserPic.NORMAL);
                             userPic.setPicUrl(userDetail.getAvatar());
