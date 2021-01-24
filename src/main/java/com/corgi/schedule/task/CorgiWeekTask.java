c9eeaf66b25dd5f38b24fe3199ce0bc8b179e073
liutairan <liu.tr@belle.com.cn>
2021-01-24 12:54:19 +0800

    添加假用户池刷新

--- src/main/java/com/corgi/schedule/task/CorgiWeekTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiWeekTask.java
@@ -24,7 +24,8 @@ public class CorgiWeekTask {
 
 
     @Async
-    @Scheduled(cron = "0 0 2 * * MON")
+    //@Scheduled(cron = "0 0 2 * * MON")
+    @Scheduled(fixedRate = 7 * 24 * 3600 * 1000)
     public void run() {
         log.info("refreshing fake user pool...........");
         corgiFakeService.refreshFakeUser(10000);
