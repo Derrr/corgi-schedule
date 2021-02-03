62daab319fef9d252824f9d2e259a6953e2be0a1
liutairan <liu.tr@belle.com.cn>
2021-02-03 11:03:53 +0800

    fix bug

--- src/main/java/com/corgi/schedule/task/CorgiBillboardTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiBillboardTask.java
@@ -43,23 +43,23 @@ import java.util.concurrent.TimeUnit;
 @Component
 @Slf4j
 public class CorgiBillboardTask {
-    @Reference(retries = 1, timeout = 60000)
+    @Reference(retries = 1, timeout = 300000)
     private CorgiBillboardService corgiBillboardService;
     @Autowired
     private MQService mqService;
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    //@Async
-    @Scheduled(fixedRate = 24 * 3600 * 1000)
-    //@Scheduled(cron = "0 0 20 * * *")
+    @Async
+    //@Scheduled(fixedRate = 24 * 3600 * 1000)
+    @Scheduled(cron = "0 0 20 * * *")
     public void run() {
         log.info("adding billboard...........");
         List<String> userIds = new ArrayList<>();
         userIds.add("8");
 
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, 2);
+        calendar.add(Calendar.DATE, 3);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String date = sdf.format(calendar.getTime());
         calendar.add(Calendar.DATE, -7);
