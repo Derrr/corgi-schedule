03123a06c0d869bc89cef197b66cea56efb9cee3
liutairan <liu.tr@belle.com.cn>
2021-02-08 12:47:26 +0800

    修改热门上线规则

--- src/main/java/com/corgi/schedule/task/CorgiHotVlogTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiHotVlogTask.java
@@ -5,11 +5,13 @@ import com.corgi.user.api.CorgiLikeService;
 import com.corgi.user.api.CorgiToolService;
 import com.corgi.user.api.CorgiVlogService;
 import com.corgi.user.entity.ActivityLike;
+import com.corgi.user.entity.CorgiVlog;
 import com.corgi.user.entity.CorgiVlogHot;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -28,7 +30,7 @@ public class CorgiHotVlogTask {
 
 
     @Async
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 0/30 * * * *")
     //@Scheduled(fixedRate = 3600 * 1000)
     public void run() {
         int page = 1;
@@ -36,7 +38,7 @@ public class CorgiHotVlogTask {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         List<String> activityList = new ArrayList<>();
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MINUTE, 90);
+        calendar.add(Calendar.MINUTE, 60);
         String hourAgo = sdf.format(calendar.getTime());
         boolean shouldContinue = true;
         CorgiVlogHot queryHot = new CorgiVlogHot();
@@ -57,23 +59,34 @@ public class CorgiHotVlogTask {
                     continue;
                 }
                 activityList.add(activityId);
+                CorgiVlog vlog = corgiVlogService.getVlog(activityId);
+                if (vlog == null || StringUtils.isEmpty(vlog.getActivityId())) {
+                    continue;
+                }
                 queryHot.setActivityId(activityId);
                 List<CorgiVlogHot> tmpList = corgiVlogService.getHotVlog(queryHot, 1, 1);
-                Long likeCount = corgiLikeService.countActivityLike(activityId);
+                Integer likeCount = corgiLikeService.countRealActivityLike(activityId);
+//                Long likeCount = corgiLikeService.countActivityLike(activityId);
+//                if (!likeCount.equals(vlog.getLikeCount())) {
+//                    CorgiVlog updateCount = new CorgiVlog();
+//                    updateCount.setActivityId(activityId);
+//                    updateCount.setLikeCount(likeCount.intValue() - vlog.getLikeCount());
+//                    corgiVlogService.addVlogCount(updateCount);
+//                }
                 if (tmpList.size() > 0) {
                     CorgiVlogHot hot = tmpList.get(0);
                     if (likeCount * 10 > hot.getExpectView()) {
                         CorgiVlogHot updateHot = new CorgiVlogHot();
                         updateHot.setId(hot.getId());
-                        updateHot.setLikeCount(likeCount.intValue());
-                        updateHot.setExpectView(likeCount.intValue() * 10);
+                        updateHot.setLikeCount(likeCount);
+                        updateHot.setExpectView(likeCount * 10);
                         corgiVlogService.updateHotVlog(updateHot);
                     }
                 } else {
                     CorgiVlogHot addHot = new CorgiVlogHot();
                     addHot.setActivityId(activityId);
-                    addHot.setExpectView(likeCount.intValue() * 10);
-                    addHot.setLikeCount(likeCount.intValue());
+                    addHot.setExpectView(likeCount * 10);
+                    addHot.setLikeCount(likeCount);
                     addHot.setType(CorgiVlogHot.TYPE.AUTO);
                     corgiVlogService.addHotVlog(addHot);
                 }
