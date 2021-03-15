70644a81b43d89f4ec8f51e81081a20e28ebec6d
liutairan <liu.tr@belle.com.cn>
2021-03-15 16:29:12 +0800

    add log

--- src/main/java/com/corgi/schedule/task/CorgiSystemMessageTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiSystemMessageTask.java
@@ -51,20 +51,20 @@ public class CorgiSystemMessageTask {
     }
 
     @Async
-    //@Scheduled(cron = "0 25 10 * * *")
-    @Scheduled(fixedRate = 1000 * 3600 * 24)
+    @Scheduled(cron = "0 25 10 * * *")
+    //@Scheduled(fixedRate = 1000 * 3600 * 24)
     public void birthdayNotice() {
         SimpleDateFormat sdf = new SimpleDateFormat("/MM/dd");
         Long time = System.currentTimeMillis() - 30 * 24 * 3600 * 1000L;
         String date = sdf.format(new Date());
-        log.info("start birthday notice:{},{} ", date,time);
+        log.info("start birthday notice:{},{} ", date, time);
         List<String> userIds = corgiUserService.getUserByBirthday(date, time);
         log.info("start birthday user:{} ", userIds.size());
         if (!CollectionUtils.isEmpty(userIds)) {
             String userId = userIds.get(0);
             UserDetail userDetail = corgiUserService.getUserDetailBasic(userId);
             log.info("push user:{} ", userDetail);
-            List<String> corgiIds = Arrays.asList("corgi1");
+            List<String> corgiIds = Arrays.asList("corgi1", "corgi2", "corgi4", "corgi7", "corgi8", "corgi9");
             SystemMessage systemMessage = new SystemMessage();
             systemMessage.setContent("你关注的 " + userDetail.getNickname() + " 今天过生日啦，快去祝贺他吧");
             systemMessage.setTitle("生日提醒");
