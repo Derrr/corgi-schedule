e8c8778d0ad7389d74885c1a3ac0b8901c5f19bd
liutairan <liu.tr@belle.com.cn>
2021-08-21 20:24:56 +0800

    修改上榜逻辑

--- src/main/java/com/corgi/schedule/task/CorgiHotVlogTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiHotVlogTask.java
@@ -95,7 +95,7 @@ public class CorgiHotVlogTask {
                         updateHot.setExpectView(expectView);
                         corgiVlogService.updateHotVlog(updateHot);
                     }
-                } else if (likeCount > 2) {
+                } else if (likeCount >= 6) {
                     CorgiVlogHot addHot = new CorgiVlogHot();
                     addHot.setActivityId(activityId);
                     addHot.setExpectView(expectView);
