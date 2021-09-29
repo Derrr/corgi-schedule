01cbb7dd28d285323721150b0e38a262c312fcb5
liutairan <liu.tr@belle.com.cn>
2021-09-29 19:17:43 +0800

    add log

--- src/main/java/com/corgi/schedule/controller/ScriptController.java
+++ src/main/java/com/corgi/schedule/controller/ScriptController.java
@@ -114,8 +114,9 @@ public class ScriptController {
     public String pushSystemMessage(@RequestParam("id") String id, @RequestParam("userId") String userId) {
         SystemMessage message = corgiSystemMessageService.getMessageDetail(id);
         HashMap extra = new HashMap();
-        extra.put("content", JSON.parse(message.getContent()));
-        //message.setContent(message.getTitle());
+        //extra.put("content", JSON.parse(message.getContent()));
+        extra.put("content", message.getContent());
+        message.setContent(message.getTitle());
         return hxPushMessageService.sendMessage(message, Arrays.asList("corgi" + userId), extra);
     }
 
