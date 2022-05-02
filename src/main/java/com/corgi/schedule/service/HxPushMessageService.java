5be337272ab739d42a39b6b00405a5de4449d5eb
liutairan <liu.tr@belle.com.cn>
2022-05-02 19:48:20 +0800

    add log

--- src/main/java/com/corgi/schedule/service/HxPushMessageService.java
+++ src/main/java/com/corgi/schedule/service/HxPushMessageService.java
@@ -91,9 +91,12 @@ public class HxPushMessageService {
 
         extra.put("type", systemMessage.getType());
         extra.put("em_apns_ext", apnsContent);
-        extra.put("title", systemMessage.getTitle());
-        extra.put("desc", systemMessage.getContent());
-        extra.put("picUrl", systemMessage.getPicUrl());
+        extra.put("alertTitle", systemMessage.getTitle());
+        if (!StringUtils.isEmpty(systemMessage.getPicUrl())) {
+            extra.put("title", systemMessage.getTitle());
+            extra.put("desc", systemMessage.getContent());
+            extra.put("picUrl", systemMessage.getPicUrl());
+        }
         extra.put("urlType", systemMessage.getUrlType());
         extra.put("url", systemMessage.getUrl());
 
