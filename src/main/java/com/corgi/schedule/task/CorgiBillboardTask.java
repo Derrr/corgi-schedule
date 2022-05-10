21eaad0dab866301511c03d98d1a45b262fdde8c
liutairan <liu.tr@belle.com.cn>
2022-05-10 14:52:58 +0800

    add log

--- src/main/java/com/corgi/schedule/task/CorgiBillboardTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiBillboardTask.java
@@ -67,7 +67,7 @@ public class CorgiBillboardTask {
         String date = new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime());
         calendar.add(Calendar.DATE, -6);
         String startTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime());
-        calendar.add(Calendar.DATE, 57);
+        //calendar.add(Calendar.DATE, -57);
         String startDate = new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime());
 
         ActivityQuery query = new ActivityQuery();
