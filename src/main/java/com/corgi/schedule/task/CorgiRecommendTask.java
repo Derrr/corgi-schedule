6c56a4a25826f2ace07383c8a2df6f54ab2fa448
liutairan <liu.tr@belle.com.cn>
2022-08-02 18:01:21 +0800

    add log

--- src/main/java/com/corgi/schedule/task/CorgiRecommendTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiRecommendTask.java
@@ -56,52 +56,60 @@ public class CorgiRecommendTask {
     @Scheduled(fixedRate = 24 * 3600 * 1000)
     public void runUser() {
         log.info("refreshing user...........");
-        List<UserPosition> userPositionList;
-        int page = 1;
-        int pageSize = 5000;
-        do {
-            userPositionList = corgiUserService.getUserPositionByPage(page, pageSize);
-            page++;
-            //log.info("page ={}, size={} ", page, userPositionList.size());
-            Long threshold = System.currentTimeMillis() - 30 * 24 * 3600 * 1000L;
-            if (userPositionList != null) {
-                for (UserPosition userPosition : userPositionList) {
-                    //log.info("checking ... " + userPosition.getUserId() + " page = " + page);
-                    List<Point> points = redisTemplate.opsForGeo().position("user", userPosition.getUserId());
-                    if (CollectionUtils.isEmpty(points)) {
-                        continue;
-                    }
-                    redisTemplate.opsForGeo().remove("user", userPosition.getUserId());
-                    if (userPosition.getLng() == null || userPosition.getLng() > 180 || userPosition.getLng() < -180) {
-                        continue;
-                    }
-                    if (userPosition.getLat() == null || userPosition.getLat() > 90 || userPosition.getLat() < -90) {
-                        continue;
-                    }
-                    if (StringUtils.isEmpty(userPosition.getUserId())) {
-                        continue;
-                    }
-                    if (userPosition.getUptime() == null) {
-                        continue;
-                    }
-                    UserDetail detail = corgiUserService.getUserDetailBasic(userPosition.getUserId());
-                    if (detail == null) {
-                        continue;
-                    }
-                    if (!"influencer".equals(detail.getAvatarStatus())) {
-                        String expire = corgiUserService.getUserVipExpire(userPosition.getUserId());
-                        if (StringUtils.isEmpty(expire) || "-".equals(expire)) {
-                            if (userPosition.getUptime() < threshold) {
-                                continue;
-                            }
-                        }
-                    }
-                    log.info("checkinginto ... " + userPosition.getUserId());
-                    redisTemplate.opsForGeo().add("user", new Point(userPosition.getLng(), userPosition.getLat()), userPosition.getUserId());
-                }
-            }
-        } while (!CollectionUtils.isEmpty(userPositionList));
-        log.info("end refreshing user...........");
+        List<Point> points = redisTemplate.opsForGeo().position("user", "42746");
+        log.info("point:{}",points);
+        redisTemplate.opsForGeo().remove("user", "42746");
+        points = redisTemplate.opsForGeo().position("user", "42746");
+        log.info("point:{}",points);
+        GeoResults<RedisGeoCommands.GeoLocation<String>> geoResults = redisTemplate.opsForGeo().radius("user", new Circle(new Point(116.410145, 39.966783), new Distance(10, Metrics.KILOMETERS)), RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs().limit(2).sortAscending());
+        log.info("geo:{}",geoResults.getContent());
+
+//        List<UserPosition> userPositionList;
+//        int page = 1;
+//        int pageSize = 5000;
+//        do {
+//            userPositionList = corgiUserService.getUserPositionByPage(page, pageSize);
+//            page++;
+//            //log.info("page ={}, size={} ", page, userPositionList.size());
+//            Long threshold = System.currentTimeMillis() - 30 * 24 * 3600 * 1000L;
+//            if (userPositionList != null) {
+//                for (UserPosition userPosition : userPositionList) {
+//                    //log.info("checking ... " + userPosition.getUserId() + " page = " + page);
+//                    List<Point> points = redisTemplate.opsForGeo().position("user", userPosition.getUserId());
+//                    if (CollectionUtils.isEmpty(points)) {
+//                        continue;
+//                    }
+//                    redisTemplate.opsForGeo().remove("user", userPosition.getUserId());
+//                    if (userPosition.getLng() == null || userPosition.getLng() > 180 || userPosition.getLng() < -180) {
+//                        continue;
+//                    }
+//                    if (userPosition.getLat() == null || userPosition.getLat() > 90 || userPosition.getLat() < -90) {
+//                        continue;
+//                    }
+//                    if (StringUtils.isEmpty(userPosition.getUserId())) {
+//                        continue;
+//                    }
+//                    if (userPosition.getUptime() == null) {
+//                        continue;
+//                    }
+//                    UserDetail detail = corgiUserService.getUserDetailBasic(userPosition.getUserId());
+//                    if (detail == null) {
+//                        continue;
+//                    }
+//                    if (!"influencer".equals(detail.getAvatarStatus())) {
+//                        String expire = corgiUserService.getUserVipExpire(userPosition.getUserId());
+//                        if (StringUtils.isEmpty(expire) || "-".equals(expire)) {
+//                            if (userPosition.getUptime() < threshold) {
+//                                continue;
+//                            }
+//                        }
+//                    }
+//                    log.info("checkinginto ... " + userPosition.getUserId());
+//                    redisTemplate.opsForGeo().add("user", new Point(userPosition.getLng(), userPosition.getLat()), userPosition.getUserId());
+//                }
+//            }
+//        } while (!CollectionUtils.isEmpty(userPositionList));
+//        log.info("end refreshing user...........");
     }
 
 
