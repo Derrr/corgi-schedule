926a1a177f3ac22030a8fc482e11fa0d288afa42
liutairan <liu.tr@belle.com.cn>
2022-09-13 16:38:06 +0800

    add log

--- src/main/java/com/corgi/schedule/task/CorgiHotVlogTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiHotVlogTask.java
@@ -108,7 +108,7 @@ public class CorgiHotVlogTask {
                 }
                 queryHot.setType(CorgiVlogHot.TYPE.MANUAL);
                 List<CorgiVlogHot> manualList = corgiVlogService.getHotVlog(queryHot, 1, 10);
-                if (!CollectionUtils.isEmpty(manualList)) {
+                if (!CollectionUtils.isEmpty(manualList) && likeCount > 0) {
                     for (CorgiVlogHot hot : manualList) {
                         CorgiVlogHot updateHot = new CorgiVlogHot();
                         updateHot.setId(hot.getId());
