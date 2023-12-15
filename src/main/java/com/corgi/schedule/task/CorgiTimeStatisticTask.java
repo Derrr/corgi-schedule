19bef761696b111623ebb815240c81b8ffcb759c
trliu <trliu@hillinsight.com>
2023-12-15 14:00:29 +0800

    创建Queue

--- src/main/java/com/corgi/schedule/task/CorgiTimeStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiTimeStatisticTask.java
@@ -42,8 +42,8 @@ public class CorgiTimeStatisticTask {
     private static SimpleDateFormat hour_sdf = new SimpleDateFormat("HH");
 
     @Async
-    //@Scheduled(cron = "0 0 0/4 * * *")
-    @Scheduled(fixedRate = 7 * 24 * 3600 * 1000)
+    @Scheduled(cron = "0 0 2/4 * * *")
+    //@Scheduled(fixedRate = 7 * 24 * 3600 * 1000)
     public void runPreferGroup() {
         log.info("into prefer group.....");
         int page = 1;
@@ -91,7 +91,7 @@ public class CorgiTimeStatisticTask {
     }
 
     @Async
-    //@Scheduled(cron = "0 0 2/4 * * *")
+    @Scheduled(cron = "0 0 0/4 * * *")
     public void runGroup() {
         corgiUserRecommendService.clearPreferCor("");
         int page = 1;
