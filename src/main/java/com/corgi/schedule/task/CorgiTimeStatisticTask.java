e0045a369aed85ba3fb9ec442464cedb90c0362f
trliu <trliu@hillinsight.com>
2023-12-22 11:03:02 +0800

    创建Queue

--- src/main/java/com/corgi/schedule/task/CorgiTimeStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiTimeStatisticTask.java
@@ -59,7 +59,7 @@ public class CorgiTimeStatisticTask {
                     Double weight = corgiUserRecommendService.getGroupWeight(groupCount, key);
                     if (weight == null) {
                         log.info("wrong weight:" + key);
-                        weight = 0.0;
+                        weight = 10000.0;
                     }
                     redisTemplate.opsForHash().put("group_weight", key, weight);
                     redisTemplate.opsForHash().put("group_count", key, groupCount);
