79f2ae72abce35bd3b481034621dd3afd97d1d4a
trliu <trliu@hillinsight.com>
2024-04-07 14:22:31 +0800

    创建Queue

--- src/main/java/com/corgi/schedule/task/CorgiHourStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiHourStatisticTask.java
@@ -82,7 +82,7 @@ public class CorgiHourStatisticTask {
         extra.put("bottomText", "去上传>>");
         extra.put("bottomUrl", userPosition.getUserId());
         extra.put("bottomUrlType", "15");
-        extra.put("title", "");
+        extra.put("title", "您可以上传微信啦");
         extra.put("desc", "");
         mqService.sendMessage(PushMessage.builder()
                 .type(PushMessage.DEFAULT)
