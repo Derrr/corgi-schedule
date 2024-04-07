5a4da16d34b5c11385def7ce5c1713a951d1b223
trliu <trliu@hillinsight.com>
2024-04-07 15:10:43 +0800

    创建Queue

--- src/main/java/com/corgi/schedule/task/CorgiHourStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiHourStatisticTask.java
@@ -56,7 +56,7 @@ public class CorgiHourStatisticTask {
 //            page++;
 //            for (UserPosition userPosition : userPositionList) {
         UserPosition userPosition = new UserPosition();
-        userPosition.setUserId("1");
+        userPosition.setUserId("7");
 //        UserDetail userDetail = corgiUserService.getUserDetailBasic(userPosition.getUserId());
 //        if (!UserDetail.VERIFIED.equals(userDetail.getAvatarCheckStatus())) {
 //            continue;
