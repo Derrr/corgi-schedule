a928d2a4dfbf84ffb9116427a370d18cb906e83e
trliu <trliu@hillinsight.com>
2024-04-11 14:22:40 +0800

    创建Queue

--- src/main/java/com/corgi/schedule/task/CorgiHourStatisticTask.java
+++ src/main/java/com/corgi/schedule/task/CorgiHourStatisticTask.java
@@ -85,7 +85,7 @@ public class CorgiHourStatisticTask {
                 extra.put("alertTitle", "您可以上传微信啦");
                 mqService.sendMessage(PushMessage.builder()
                         .type(PushMessage.DEFAULT)
-                        .sourceUserId("corgi4")
+                        .sourceUserId("corgihelper")
                         .targetUserId(userPosition.getUserId())
                         .message("恭喜！你已满足上传微信的条件，现在去上传可赚取零花钱哦~")
                         .extra(extra)
